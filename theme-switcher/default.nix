80337a53dc3983cee15b756f433a2b846851ae10
Colum Cross <columcross@gmail.com>
2026-07-29 17:09:46 +0200

    fix: preserve theme runtime dependencies and icons

--- theme-switcher/default.nix
+++ theme-switcher/default.nix
@@ -7,7 +7,7 @@ let
 
   toggleTheme = pkgs.writeShellApplication {
     name = "toggle-theme";
-    runtimeInputs = with pkgs; [ coreutils procps libnotify dconf hyprland kitty ];
+    runtimeInputs = with pkgs; [ coreutils procps libnotify dconf dunst hyprland hyprpaper kitty ];
     text = builtins.readFile ./toggle-theme.sh;
   };
 
@@ -50,7 +50,7 @@ in
   xdg.configFile."theme-switcher/themes/dark/wallpaper.jpg".source = ../wallpapers/dark_wallpaper.jpg;
   xdg.configFile."theme-switcher/themes/dark/rofi-config.rasi".source = ./themes/dark/rofi-config.rasi;
   xdg.configFile."theme-switcher/themes/dark/gtk.css".source = ./themes/dark/gtk.css;
-  xdg.configFile."theme-switcher/themes/dark/wlogout.css".source = ./themes/dark/wlogout.css;
+  xdg.configFile."theme-switcher/themes/dark/wlogout.css".text = builtins.replaceStrings [ "@WLOGOUT_ICONS@" ] [ "${pkgs.wlogout}/share/wlogout/icons" ] (builtins.readFile ./themes/dark/wlogout.css);
   xdg.configFile."theme-switcher/themes/light/kitty.conf".source = ./themes/light/kitty.conf;
   xdg.configFile."theme-switcher/themes/light/active-border".source = ./themes/light/active-border;
   xdg.configFile."theme-switcher/themes/light/inactive-border".source = ./themes/light/inactive-border;
@@ -58,7 +58,7 @@ in
   xdg.configFile."theme-switcher/themes/light/wallpaper.jpg".source = ../wallpapers/light_wallpaper.jpg;
   xdg.configFile."theme-switcher/themes/light/rofi-config.rasi".source = ./themes/light/rofi-config.rasi;
   xdg.configFile."theme-switcher/themes/light/gtk.css".source = ./themes/light/gtk.css;
-  xdg.configFile."theme-switcher/themes/light/wlogout.css".source = ./themes/light/wlogout.css;
+  xdg.configFile."theme-switcher/themes/light/wlogout.css".text = builtins.replaceStrings [ "@WLOGOUT_ICONS@" ] [ "${pkgs.wlogout}/share/wlogout/icons" ] (builtins.readFile ./themes/light/wlogout.css);
   xdg.configFile."theme-switcher/themes/light/dunst.conf".source = ./themes/light/dunst.conf;
   xdg.configFile."theme-switcher/themes/dark/rofi-theme.rasi".source = ./themes/dark/rofi-theme.rasi;
   xdg.configFile."theme-switcher/themes/light/rofi-theme.rasi".source = ./themes/light/rofi-theme.rasi;
